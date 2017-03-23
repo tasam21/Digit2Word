131b516998b181c75596b96a3fbb6ef1d475379a
tashi samphel <root@Tashis-MacBook-Air.local>
2017-03-23 14:58:46 -0700

    Added Javascript File and Made some changes in index page and style.css

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
 <title>Digit-to-Word</title>
 </head>
 
@@ -20,22 +21,39 @@
 		<article>
 			<header>
 				<h2>Language Used:</h2>
-				<p>HTML5 | CSS3 | Bootstrap | JavaScript | JQuery | PHP | MySQL</p>
+				<p>HTML5 | CSS3 | Bootstrap | JavaScript </p>
 			
 		</article>
 		
-		<article>
-			
-		</article>
+		<div class="col-md-4">
+			<form>
+	  				<div class="form-group">
+    					<label for="exampleInputEmail1">Amount in Number</label>
+    						<input type="text" class="form-control" id="num" placeholder="Amount in Number Format">
+    						<small id="emailHelp" class="form-text text-muted">Write your donation amout in number format.</small>
+  					</div>
+					<div class="form-group">
+  						<input type="button" class="btn btn-primary" name="sr1" value="Convert into Word" onClick="numinwrd()">
+  					</div>
+
+  					<div class="form-group">
+    					<label for="amoutinword">Amount in Word Format</label>
+    						<input type="text" class="form-control" id="number" placeholder="Amount in Word Format">
+  					</div>
+  					
+			</form>
+
+
+		</div>
 		
 	</section>
 
-	
+	<div class="col-md-12">
 
 	<footer>
 		<p>Copyright &copy 2017: All right reserved to Tashi Samphel</p>
 	</footer>
-
+	</div>
+<script src="js/conversion.js" type="text/javascript"></script>
 </body>
-
 </html>
