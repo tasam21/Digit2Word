89ce11e4b04f1416a5b3727fc2f2ccad06198ccd
tashi samphel <root@Tashis-MacBook-Air.local>
2017-03-23 13:34:30 -0700

    Added index.php first page

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Your Website</title>
+</head>
+
+<body>
+
+	<header>
+		<h1>Conversion of Amount in Digit to Word Format</h1>
+	</header>
+	
+	<section>
+	
+		<article>
+			<header>
+				<h2>Language Used:</h2>
+				<p>HTML5 | CSS3 | Bootstrap | JavaScript | JQuery | PHP | MySQL</p>
+			
+		</article>
+		
+		<article>
+			
+		</article>
+		
+	</section>
+
+	
+
+	<footer>
+		<p>Copyright &copy 2017: All right reserved to Tashi Samphel</p>
+	</footer>
+
+</body>
+
+</html>
